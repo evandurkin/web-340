a620b3156895a681dc184d489824e5ed64d34e5b
Evan Durkin <edurkin@my365.bellevue.edu>
2021-08-17 20:38:17 -0400

    added code comments

--- week-1/durkin-exercise-1.3.js
+++ week-1/durkin-exercise-1.3.js
@@ -3,10 +3,13 @@
 // Date: 14 August 2021
 // Description: Demonstrates use of accessing URL through Node.js module
 
+// uses the URL module and assigns it to a variable
 var url = require("url");
 
+// parses the URL string and assigns it to a variable
 var parsedURL = url.parse("https://www.testing.com/profile?name=durkin");
 
+// calls the parsed elements of the URL string with parsedURL variable
 console.log(parsedURL.protocol);
 console.log(parsedURL.host);
 console.log(parsedURL.query);
