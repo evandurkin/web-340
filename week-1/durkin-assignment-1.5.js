a89de1c92befd7436b34380695f4d0bfe9c4b84d
Evan Durkin <edurkin@my365.bellevue.edu>
2021-08-20 14:35:22 -0400

    updated 1.5 with code comments

--- week-1/durkin-assignment-1.5.js
+++ week-1/durkin-assignment-1.5.js
@@ -3,22 +3,24 @@
 // Date: 14 August 2021
 // Description: demonstrates sending a response header to HTTP request 
 
+//assigns the http module to a variable
 var http = require("http");
 
+//web browser request and respond function
 function processRequest(req, res) {
-    var body = "Evan's Assignment";
+    var body = "Evan's Assignment"; //response to client
 
     var contentLength = body.length;
-    res.writeHead(200, {
+    res.writeHead(200, { //writes the response to client
         "Content-Length": contentLength,
         "Content-Type": "text/plain"
     });
 
-    res.end(body);
+    res.end(body); //ends the response
 
 }
 
-var s = http.createServer(processRequest);
+var s = http.createServer(processRequest); //assigns the server to a variable
 
-s.listen(8080);
+s.listen(8080); //server listens on port 8080
 
